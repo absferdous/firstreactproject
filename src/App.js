c25cf497125669ce097d9042ffbf10dd630ed378
absferdous <aferdous053@gmail.com>
2023-07-30 15:14:45 +0600

    changed again

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ function calculateWinner(squares) {
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
-    ];//winning logic
+    ];//winning 
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
